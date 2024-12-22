9176d628ddefdbc5d35e9d8c15f5fc4f51c06204
Sasha Blashenkov <git-commits@trall.co>
2024-12-22 16:36:04 +0000

    Handle coffeescript absence

--- lib/asset_gluer/javascript_file.rb
+++ lib/asset_gluer/javascript_file.rb
@@ -28,13 +28,36 @@ module AssetGluer
     protected
       def process_with_coffee(contents)
         Rails.cache.fetch "gluer:js:coffee:#{Digest::SHA1.hexdigest(contents)}", :expires_in => 1.day do
-          IO.popen 'coffee -bsc', 'r+' do |io|
-            io.write(contents)
-            io.close_write
-            io.read
+          begin
+            coffee_path = which_coffee
+            raise "CoffeeScript compiler not found. Please install it with 'npm install -g coffee-script'" unless coffee_path
+
+            IO.popen "#{coffee_path} -bsc", 'r+' do |io|
+              io.write(contents)
+              io.close_write
+              io.read
+            end
+          rescue Errno::ENOENT => e
+            Rails.logger.error "CoffeeScript compilation failed: #{e.message}"
+            raise "CoffeeScript compiler not found. Please install it with 'npm install -g coffee-script'"
+          rescue => e
+            Rails.logger.error "CoffeeScript compilation failed: #{e.message}"
+            raise "Failed to compile CoffeeScript: #{e.message}"
           end
         end
       end
+
+      private
+        def which_coffee
+          exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+          ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+            exts.each do |ext|
+              coffee = File.join(path, "coffee#{ext}")
+              return coffee if File.executable?(coffee) && !File.directory?(coffee)
+            end
+          end
+          nil
+        end
 
   end
 end
